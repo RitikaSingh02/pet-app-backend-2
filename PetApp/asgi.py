2cb939645992f90a6a71bdc8986ad75d0412d34a
RitikaSingh02 <ritika2002singh@gmail.com>
2021-10-31 18:22:56 +0530

    add asgi req

--- PetApp/asgi.py
+++ PetApp/asgi.py
@@ -1,7 +1,10 @@
 import os
-
-from django.core.asgi import get_asgi_application
-
+import django
+from channels.routing import get_default_application
+# from django.core.asgi import get_asgi_application
+import channels.layers
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetApp.settings')
 
-application = get_asgi_application()
+application = get_default_application()
+django.setup()
+channel_layer = channels.layers.get_channel_layer()
