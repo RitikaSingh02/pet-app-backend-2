04e820ae7e9125174d5f6efca7607ff163e3226e
RitikaSingh02 <ritika2002singh@gmail.com>
2021-11-01 18:09:23 +0530

    added model for forums

--- Chat/models.py
+++ Chat/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from Owner.models import ForumInfo
 
 class Message(models.Model):
     author = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     timestamp = models.DateField(auto_now_add=True, blank=True , null=True)
+
+class ForumMessage(models.Model):
+    author = models.ForeignKey(User, related_name='forummessages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateField(auto_now_add=True, blank=True , null=True)
+    forum = models.ForeignKey(ForumInfo, related_name='messages', on_delete=models.CASCADE)
 # Create your models here.
