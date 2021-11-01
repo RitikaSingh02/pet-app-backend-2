93b95d3e6cb3dc13d6afb1f9fe9070e1b2dbc52a
RitikaSingh02 <ritika2002singh@gmail.com>
2021-11-01 18:10:04 +0530

    added api for forum messages

--- Chat/urls.py
+++ Chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path , re_path
 
-from .views import chat
+from .views import chat , forum
 
 urlpatterns = [
     re_path(r"^chat/(?P<username>\w+)/$" , chat),
+    re_path(r"^chat/forum/(?P<forumid>\w+)/$" , forum),
     # path('accounts/', include('Login.urls')),
 
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.dispatch.dispatcher import receiver
 from django.shortcuts import render
-from .models import Message
+
+from .models import Message,ForumMessage
 from django.db.models import Q
 
 def chat(request , username):
@@ -15,7 +16,22 @@ def chat(request , username):
     msg_query = list(Message.objects.filter(Q(author_id = request.user.id , receiver_id = receiver_id) | Q(receiver_id = request.user.id , author_id = receiver_id)).values('message'))
     msgs = []
     for msg in msg_query:
-        msgs.append(msg['message'])
+        msgs.append([msg['message'] , "receiver: " + username ,  "sender: " + request.user ])
     params["msgs"] = msgs
+    return render(request , "chat.html" , params )
+
+def forum(request , forumid):
+    params = {
+        "forum" : forumid , 
+        "sender" : request.user ,
+        }
+    msg_query = list(ForumMessage.objects.filter(forum_id = forumid).values('message' , 'author_id' , 'receiver_id'))
+    msgs = []
+    for msg in msg_query:
+        receiver = User.objects.get(id = msg['receiver_id']).username
+        sender = User.objects.get(id = msg['author_id']).username
+        msgs.append([msg['message'] , "receiver:"  + receiver , "sender: " + sender])
+    params["msgs"] = msgs
+    print(msgs)
     return render(request , "chat.html" , params )
 # Create your views here.
