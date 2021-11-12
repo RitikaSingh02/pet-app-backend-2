a4a8dc6298be25347eb5f88d6d7438eebe9078d3
RitikaSingh02 <ritika2002singh@gmail.com>
2021-11-13 00:47:55 +0530

    --add checks for forums and response formatting

--- Chat/views.py
+++ Chat/views.py
@@ -2,9 +2,12 @@ from django.contrib import auth
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.dispatch.dispatcher import receiver
+from django.http.response import JsonResponse
 from django.shortcuts import render
 
 from .models import Message,ForumMessage
+from Owner.models import UserForum
+
 from django.db.models import Q
 
 def chat(request , username):
@@ -16,11 +19,20 @@ def chat(request , username):
     msg_query = list(Message.objects.filter(Q(author_id = request.user.id , receiver_id = receiver_id) | Q(receiver_id = request.user.id , author_id = receiver_id)).values('message'))
     msgs = []
     for msg in msg_query:
-        msgs.append([msg['message'] , "receiver: " + username ,  "sender: " + request.user ])
+        msgs.append({ "message": msg['message'] , "receiver" : str(username) ,  "sender" : str(request.user) })
+        # msgs.append(["message: " +  msg['message'] , "receiver :" + str(username) ,  "sender : "+ str(request.user) ]) #for template
+
     params["msgs"] = msgs
-    return render(request , "chat.html" , params )
+    # return render(request , "chat.html" , params )
+    return JsonResponse(params["msgs"] , safe = False , status = 200)
 
 def forum(request , forumid):
+    #custom check for the user existing in the forum
+    query = UserForum.objects.filter(forum_id = forumid , user_id = request.user.id).exists()
+    if not query:
+        res = {}        
+        res['msg'] = "You are unauthorized to access this forum"
+        return JsonResponse(res , safe=False , status = 401)
     params = {
         "forum" : forumid , 
         "sender" : request.user ,
@@ -30,8 +42,11 @@ def forum(request , forumid):
     for msg in msg_query:
         receiver = User.objects.get(id = msg['receiver_id']).username
         sender = User.objects.get(id = msg['author_id']).username
-        msgs.append([msg['message'] , "receiver:"  + receiver , "sender: " + sender])
+        msgs.append({ "message": msg['message'] , "receiver" : str(receiver) ,  "sender" : str(sender) })
+        # msgs.append(["message: " +  msg['message'] , "receiver :" + str(receiver) ,  "sender : "+ str(sender) ]) #for template
     params["msgs"] = msgs
-    print(msgs)
-    return render(request , "chat.html" , params )
+    # print(msgs)
+    # return render(request , "chat.html" , params )
+    return JsonResponse(params["msgs"] , safe = False , status = 200)
+
 # Create your views here.
