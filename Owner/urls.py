9b8a167e04da1b170a310b47f93b72a92f335cb3
RitikaSingh02 <ritika2002singh@gmail.com>
2021-11-13 22:56:10 +0530

    --add forum_add endpoint

--- Owner/urls.py
+++ Owner/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
-from .views import forum_create
+from .views import forum_add, forum_create
 
 urlpatterns = [
     path('create/', forum_create),
+    path('add/', forum_add),
+
 ]
--- Owner/views.py
+++ Owner/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.http.response import JsonResponse
+from django.contrib.auth.decorators import login_required
+
 import json
 
 from .models import UserForum , ForumInfo
 
+@login_required
 def forum_create(request):
     res = {}
     if request.method == "POST":
@@ -32,7 +35,22 @@ def forum_create(request):
         return JsonResponse(res , safe=False , status = 405)
     res['msg'] = "Forum Created Successfully"
     return JsonResponse(res , safe=False , status = 200)
-    
 
-
+@login_required
+def forum_add(request):
+    res = {}
+    if request.method == "POST":
+        data = json.loads(request.body)
+        user_id_list = data['user_id_list']
+        forum_id = data['forum_id']
+        for user_id in user_id_list:
+            UserForum.objects.create(
+                forum_id = forum_id,
+                user_id = user_id
+            )
+    else:
+        res['msg'] = "Method not allowed"
+        return JsonResponse(res , safe=False , status = 405)
+    res['msg'] = "User/s added to the forum Successfully"
+    return JsonResponse(res , safe=False , status = 200)    
 
