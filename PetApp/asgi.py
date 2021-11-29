9f4bfa6379ff2ced694cb289e8686fb51cf70221
RitikaSingh02 <ritika2002singh@gmail.com>
2021-11-29 22:22:07 +0530

    --update layers config

--- PetApp/asgi.py
+++ PetApp/asgi.py
@@ -1,10 +1,18 @@
+# import os
+# import django
+# from channels.routing import get_default_application
+# # from django.core.asgi import get_asgi_application
+# # import channels.layers
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetApp.settings')
+
+# application = get_default_application()
+# django.setup()
+# # channel_layer = channels.layers.get_channel_layer()
+
 import os
 import django
 from channels.routing import get_default_application
-# from django.core.asgi import get_asgi_application
-# import channels.layers
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetApp.settings')
 
-application = get_default_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PetApp.settings")
 django.setup()
-# channel_layer = channels.layers.get_channel_layer()
+application = get_default_application()
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: daphne PetApp.asgi:application --port $PORT --bind 0.0.0.0 -v2
-worker: python manage.py runworker channels --settings=PetApp.settings -v2
+chatworker: python manage.py runworker --settings=PetApp.settings -v2
